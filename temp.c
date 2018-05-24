1a4eb404608e95e2a40c094d06caf44975fe60a1
Nodir <sinigami83@mail.ru>
2018-05-24 10:42:21 +0500

    changed temp

--- temp.c
+++ temp.c
@@ -4,6 +4,7 @@ int main()
 {
 	int a, b, x;
 	int max;
+	int i;
 	/*scanf("%d %d", &a, &b);*/
 	
 	a = b = x = 5;
@@ -14,5 +15,37 @@ int main()
 	max = a > b ? a : b;
 
 	printf("%d\n", max);
+
+	if (a > b) {
+		max = a;
+	}
+	else {
+		max = b;
+	}
+	printf("%d\n", max);
+
+	switch (a < b) 
+	{
+		case 
+			4: printf("%d\n", a);
+			break;
+		case
+			6: printf("%d\n", b);
+			break;
+		default:
+			printf("Error");
+			break;
+	}
+
+	while (a >= b) {
+		++b;
+		printf("%d\t", b);
+	}
+	printf("\n", b);
+
+	printf("Table\n");
+	
+	for (i = 0; i <= 25; i++)
+		printf("%d x %d\t = %d\n", i, i, i*i);
 	return 0;
 }
